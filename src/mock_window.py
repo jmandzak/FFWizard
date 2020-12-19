0cbf404c9fed2863bcaa984de0070d4ad4d1b22d
jmandzak <jmandzak@vols.utk.edu>
2020-12-19 13:44:28 -0500

    Added in user team to see

--- src/mock_window.py
+++ src/mock_window.py
@@ -44,9 +44,35 @@ class MockWindow(QWidget):
             if draft_order[i] == "Your Team":
                 self.drafting_teams.item(0, i).setBackground(QBrush(QColor("Yellow")))
 
+        self.my_team = QTableWidget()
+        self.my_team.setRowCount(16)
+        self.my_team.setColumnCount(2)
 
+        # set up the table
+        self.my_team.setItem(0, 0, QTableWidgetItem("QB"))
+        self.my_team.setItem(1, 0, QTableWidgetItem("RB"))
+        self.my_team.setItem(2, 0, QTableWidgetItem("RB"))
+        self.my_team.setItem(3, 0, QTableWidgetItem("WR"))
+        self.my_team.setItem(4, 0, QTableWidgetItem("WR"))
+        self.my_team.setItem(5, 0, QTableWidgetItem("TE"))
+        self.my_team.setItem(6, 0, QTableWidgetItem("FLEX"))
+        self.my_team.setItem(7, 0, QTableWidgetItem("DEF"))
+        self.my_team.setItem(8, 0, QTableWidgetItem("K"))
+        for i in range(8):
+            self.my_team.setItem(i+9, 0, QTableWidgetItem("BE"))
+
+        self.my_team.setColumnWidth(0, 50)
+        self.my_team.setColumnWidth(1, 400)
+        #self.my_team.horizontalHeader().setVisible(False)
+        self.my_team.verticalHeader().setVisible(False)
+        self.my_team.setHorizontalHeaderLabels(["", "Your Team"])
+
+
+        self.team_player_split.addWidget(self.my_team)
 
         self.main_layout.addWidget(self.drafting_teams)
+        self.main_layout.addSpacing(100)
+        self.main_layout.addLayout(self.team_player_split)
         self.setLayout(self.main_layout)
 
         
